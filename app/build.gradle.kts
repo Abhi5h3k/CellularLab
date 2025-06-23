f144db32f7e5a62bce470338fa482b7d4b8eabae
Abhishek Bhardwaj <abhishekbhardwajca@gmail.com>
2025-06-23 16:58:10 +0530

    🔧 Bump versionName to 1.6 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.5"
+        versionName = "1.6"
         buildToolsVersion = "36.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
